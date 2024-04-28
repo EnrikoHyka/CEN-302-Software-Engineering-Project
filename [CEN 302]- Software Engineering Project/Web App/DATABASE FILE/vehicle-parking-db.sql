1572b1d15fc1c4438d3a1622b3cb213777583f85
EnrikoHyka <149476808+EnrikoHyka@users.noreply.github.com>
2024-04-28 18:39:09 +0200

    Updates

---
+++ [CEN 302]- Software Engineering Project/Web App/DATABASE FILE/vehicle-parking-db.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 17, 2021 at 07:51 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vehicle-parking-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Security_Code` int(55) NOT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Security_Code`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Administrator', 'admin', 7854445410, 1100, 'admin@gmail.com', 'd00f5d5217896fb7fd601412cb890830', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL,
+  `c_name` varchar(255) NOT NULL,
+  `c_email` varchar(55) NOT NULL,
+  `c_website` varchar(55) NOT NULL,
+  `c_address` varchar(255) NOT NULL,
+  `last_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `c_name`, `c_email`, `c_website`, `c_address`, `last_update`) VALUES
+(1, 'Epoka Parking', 'eparking@company.com', 'epokaparking.com', 'Rinas, Tirane', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vcategory`
+--
+
+CREATE TABLE `vcategory` (
+  `ID` int(10) NOT NULL,
+  `VehicleCat` varchar(120) DEFAULT NULL,
+  `shortDescription` varchar(50) NOT NULL,
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vcategory`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle_info`
+--
+
+CREATE TABLE `vehicle_info` (
+  `ID` int(10) NOT NULL,
+  `ParkingNumber` varchar(120) DEFAULT NULL,
+  `VehicleCategory` varchar(120) NOT NULL,
+  `VehicleCompanyname` varchar(120) DEFAULT NULL,
+  `RegistrationNumber` varchar(120) DEFAULT NULL,
+  `OwnerName` varchar(120) DEFAULT NULL,
+  `OwnerContactNumber` bigint(10) DEFAULT NULL,
+  `InTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `OutTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `ParkingCharge` varchar(120) NOT NULL,
+  `Remark` mediumtext NOT NULL,
+  `Status` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vehicle_info`
+--
+
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vcategory`
+--
+ALTER TABLE `vcategory`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `vehicle_info`
+--
+ALTER TABLE `vehicle_info`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `vcategory`
+--
+ALTER TABLE `vcategory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `vehicle_info`
+--
+ALTER TABLE `vehicle_info`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ [CEN 302]- Software Engineering Project/Web App/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="col-sm-12">
+		<p class="back-link">&copy; <?php echo date("Y"); ?> - Developed By Enriko Hyka, Erik Veshi, Enad Draçini, Enes Musiçi, Serxhio Elezi, Klajdi Gazidedja, Isgen Tetova</p>
+	</div>
+</footer>
